3c72474e7f7a0dea36f082fd578a088bed0c6d26
Apprentice <apprentice@devbootcamp.com>
2013-07-25 14:25:05 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whestone::Application.config.secret_token = '321b10c31ffc673229b60f4292d711f5ac813be882d5857dbc76eced97dfbb15f47a8122178e7b7f2041c107c6a196a301528f2afdac41e45ff81b903fe56be8'

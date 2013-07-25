f84272cf2190085b202222f183d058877a40535c
Jared Smith <jared.rit@me.com>
2013-07-25 15:42:25 -0700

    Update user model to have relation to stones

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+  has_and_belongs_to_many :stones
 end

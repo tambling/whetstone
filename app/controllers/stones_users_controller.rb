f101d159f7119cd20160d685c6b54159b4320c59
Dean Tambling <dean.tambling@gmail.com>
2013-07-26 12:05:14 -0700

    Add Goal button triggers AJAX event to add StonesUser.

--- app/controllers/stones_users_controller.rb
+++ app/controllers/stones_users_controller.rb
@@ -1,7 +1,10 @@
 class StonesUsersController < ApplicationController
   def create
-    current_user.goals << StonesUser.new(goal_id)
+    current_user.stones << Stone.find(params[:stone_id])
+    current_user.goals.last.timelines << Timeline.new
+    redirect_to stone_path(params[:stone_id])
   end
   def new
+    @stone = Stone.find(params[:stone_id])
   end
 end

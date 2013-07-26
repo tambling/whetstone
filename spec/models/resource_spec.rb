a559f39a101e7bb7d6ba0be83278314a8c9f5a25
Jared Smith <jared.rit@me.com>
2013-07-25 17:20:32 -0700

    Make resource specs pass

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe Resource do
 
-	let!(:resource) { FactoryGirl.build(:resource) }
+	let(:resource) { FactoryGirl.build(:resource) }
 
 	context "Validations" do
 		it { should validate_presence_of(:title) }
-		it { should validate_presence_of(:content) }
+		it { should validate_presence_of(:description) }
 		it { should validate_presence_of(:recommended_time) }
-		it { should belong_to(:stone) }
+		it { should have_and_belong_to_many(:stones) }
 		it { should have_many(:votes) }
 	end
 

243817479d2b5aa66f70309593bbbb57589c52ea
Jared Smith <jared.rit@me.com>
2013-07-26 15:51:34 -0700

    Fix vote being updated when the controller does not permit a vote happening more than once per user per resource per stone

--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -1,9 +1,23 @@
 $(document).ready( function() {
+	var $votes;
+	var catchNumbers = /\d+/
+
+
 	$('.resources').delegate(".upvote","click",function() {
-		$.ajax({ 
-			type: 'POST', 
+		$votes = $(this.parentElement)
+		$.ajax({
+			type: 'POST',
 			url: '/votes', 
-			data: {'relationship_id' : this.id, 'value' : 1}
-		});
+			data: {'relationship_id' : this.id, 'value' : 1} 
+			})
+		.done(function() { updateVotes() });
 	});
+	
+	function updateVotes() {
+			var voteCount = (parseInt(catchNumbers.exec($votes.eq(0).find('p').eq(0).text())) + 1).toString() + " votes";
+			$votes.eq(0).find('p').eq(0).text(voteCount)
+	}
+
 });
+
+

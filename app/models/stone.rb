8a7ec24aa417bca97583504a1cdc2bf50d599a99
Jared Smith <jared.rit@me.com>
2013-07-25 19:40:32 -0700

    Add reference to votes from stone model

--- app/models/stone.rb
+++ app/models/stone.rb
@@ -1,9 +1,12 @@
 class Stone < ActiveRecord::Base
+  attr_accessible :title, :description
+	
 	validates_uniqueness_of :title
 	validates_presence_of :title
 	validates_presence_of :description
-  attr_accessible :title, :description
+
+	has_many :votes
+
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many :resources
-
 end

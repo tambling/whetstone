9afc2a99e9add0e8fc9b4175b0eceee145182911
Jared Smith <jared.rit@me.com>
2013-07-25 17:06:02 -0700

    Stone rspecs pass

--- app/models/stone.rb
+++ app/models/stone.rb
@@ -1,4 +1,9 @@
 class Stone < ActiveRecord::Base
+	validates_uniqueness_of :title
+	validates_presence_of :title
+	validates_presence_of :description
+
+
 	has_and_belongs_to_many :users
 	has_and_belongs_to_many :resources
 

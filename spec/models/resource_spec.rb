89e1fc35173a583bf66656eab1d1890fbcc115f3
Jared Smith <jared.rit@me.com>
2013-07-26 17:55:15 -0700

    Deleted unecessary test checking :recommended_time to be true, changed has_and_belongs_to_many relationship to two has_many tests

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -7,8 +7,8 @@ describe Resource do
 	context "Validations" do
 		it { should validate_presence_of(:title) }
 		it { should validate_presence_of(:description) }
-		it { should validate_presence_of(:recommended_time) }
-		it { should have_and_belong_to_many(:stones) }
+		it { should have_many(:stones) }
+		it { should have_many(:resources_stones) }
 	end
 
 end

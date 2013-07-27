47ba1ba034e44f597166381c1daa6e1da6fae09f
Jared Smith <jared.rit@me.com>
2013-07-26 19:15:38 -0700

    Create tests for votes controller

---
+++ spec/controllers/votes_controlller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe VotesController, "creating a new vote" do
+	integrate_views
+	fixtures :votes 
+
+	it "should return an arbitrary page if save is successful" do
+
+	it "should return a 403 code on failed save"
+
+end

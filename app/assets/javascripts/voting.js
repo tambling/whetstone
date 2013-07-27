f285619ef9d448a40f7b57b78f14c3e2305846c1
Jared Smith <jared.rit@me.com>
2013-07-26 17:38:37 -0700

    Fix naming convention for vote attributes, refactored code to delete duplicate code where calls to the server for voting are made.

--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -5,32 +5,22 @@ $(document).ready( function() {
 
 	$('.resources').delegate(".upvote","click",function() {
 		$votes = $(this.parentElement)
-		$.ajax({
-			type: 'POST',
-			url: '/votes', 
-			data: {'relationship_id' : this.id, 'value' : 1} 
-		})
-		.done(function() { updateVotes(1) });
+		postNewVote(this, 1)
 	});
 
 	$('.resources').delegate(".downvote","click",function() {
 		$votes = $(this.parentElement)
+		postNewVote(this, -1)
+	});
+	
+	function postNewVote(link, val) {
 		$.ajax({
 			type: 'POST',
 			url: '/votes', 
-			data: {'relationship_id' : this.id, 'value' : -1} 
+			data: {'resources_stone_id' : link.id, 'value' : val} 
 		})
-		.done(function() { updateVotes(-1) });
-	});
-	
-	// function postNewVote(val) {
-	// 	$.ajax({
-	// 		type: 'POST',
-	// 		url: '/votes', 
-	// 		data: {'relationship_id' : this.id, 'value' : val} 
-	// 	})
-	// 	.done(function() { updateVotes(val) });
-	// }
+		.done(function() { updateVotes(val) });
+	}
 
 	function updateVotes(val) {
 		var voteCount = (parseInt(catchNumbers.exec($votes.eq(0).find('p').eq(0).text())) + val).toString() + " votes";

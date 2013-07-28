67c76b820e01a25009fa7babc619e8671be33322
Jared Smith <jared.rit@me.com>
2013-07-27 19:13:38 -0700

    Change comment model to be associated with votes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
 
   validates :content, presence: true
 
+  has_many :votes, as: :voteable
   has_many :comments, as: :commentable
   
   belongs_to :commentable, polymorphic: true

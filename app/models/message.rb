215f60d03103332e32c98b49cf226b26a782643f
Dean Tambling <dean.tambling@gmail.com>
2013-07-28 17:07:46 -0700

    Improved useability for messages.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,6 @@ class Message < ActiveRecord::Base
   belongs_to :from, :class_name => "User", :foreign_key => "from_id"
   belongs_to :to, class_name: "User", foreign_key: "to_id"
   attr_accessible :body, :from_id, :to_id
+
+  validates :body, :from_id, :to_id, presence: true
 end

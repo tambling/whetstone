ca089cdcbd09b1c80c4d17e35b7ccecbe3b67db4
Jared Smith <jared.rit@me.com>
2013-07-30 15:51:50 -0700

    Refactor controller code

--- app/controllers/saved_resources_controller.rb
+++ app/controllers/saved_resources_controller.rb
@@ -1,22 +1,27 @@
 class SavedResourcesController < ApplicationController
+  
   def create
+
     unless user_signed_in?
-      render :json => "You need to sign in to save resources".to_json
-    else
-      resources_stone = ResourcesStone.find(params[:resources_stone_id])
-      goal = current_user.goals.find_or_create_by_stone_id(resources_stone.stone.id)
-
-
-      saved_resource = SavedResource.new(stones_user_id: goal.id, resources_stone_id: resources_stone.id)
-
-      if saved_resource.valid?
-        saved_resource.save
-        goal.queue+=", #{saved_resource.id}"
-        goal.save
-        render :json => "Creation Successful".to_json
-      else
-        render :json => "Resource has already been added".to_json
-      end
+      render nothing: true, status: :unauthorized
+      return
     end
+
+    resources_stone = ResourcesStone.find(params[:resources_stone_id])
+    goal = current_user.create_goal_for(resources_stone.stone)
+
+    saved_resource = SavedResource.new(stones_user_id: goal.id, resources_stone_id: resources_stone.id)
+
+    if saved_resource.save
+      goal.queue += ", #{saved_resource.id}"
+      goal.save
+      render json: saved_resource, status: :created
+      return
+    end
+    
+    render nothing: true, status: :unprocessable_entity
+
   end
+
+
 end

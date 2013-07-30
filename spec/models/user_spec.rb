25cdc9c78f13a01219515b9dee38e90687d5930a
Dean Tambling <dean.tambling@gmail.com>
2013-07-30 15:39:17 -0700

    Removed a test for code that's no longer there.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,13 +10,13 @@ describe User do
 
 	context "Validations" do
 		it { should validate_presence_of(:name) }
-		it { should validate_presence_of(:email) } 
-		it { should validate_presence_of(:password) } 
+		it { should validate_presence_of(:email) }
+		it { should validate_presence_of(:password) }
 		it { should have_many(:stones) }
 	end
 
 	context "Methods" do
-		
+
 		it "should retrieve messages made with another user" do
 			expect(user.messages_with(user2).size).to eq(1)
 		end
@@ -30,10 +30,5 @@ describe User do
 			expect(user.message_partners.size).to eq(1)
 			expect(user2.message_partners.size).to eq(1)
 		end
-
-		it "should get the next resource to be completed" do 
-			expect(user.next_queued_resource).to eq(saved_resource)
-		end
-
 	end
 end

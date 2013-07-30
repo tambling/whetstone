b80d38cdd96499dcc82d7ae5ad9b5037c6ae922e
Dean Tambling <dean.tambling@gmail.com>
2013-07-30 14:35:30 -0700

    Adjusted resource management spec to reflect recent style changes.

--- spec/integrations/resource_management_spec.rb
+++ spec/integrations/resource_management_spec.rb
@@ -17,7 +17,7 @@
 
     scenario "User Visits Resources Page" do
       click_link("Resources")
-      expect(page).to have_content(resource.title)
+      expect(page).to have_content(resource.title.upcase)
     end
 
     scenario "User Visits Resources Page And Can Add a Resource" do
@@ -30,7 +30,7 @@
 
       page.find(:css,'.submit_button input').click
 
-      expect(page).to have_content("Poodr")
+      expect(page).to have_content("Poodr".upcase)
     end
 
   end

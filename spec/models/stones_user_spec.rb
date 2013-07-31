9da52211989aa3df2c22cda1301d1411a6d60c5f
Dean Tambling <dean.tambling@gmail.com>
2013-07-30 18:48:19 -0700

    Added very basic testing code targeting one particular line.

--- spec/models/stones_user_spec.rb
+++ spec/models/stones_user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe StonesUser do
 
-  let(:goal) { FactoryGirl.create(:stones_user) }
+  let(:goal) { FactoryGirl.create(:stones_user, queue: "1, 2") }
   let(:saved_resource) { FactoryGirl.create(:saved_resource, stones_user: :goal) }
 
   context "Validations" do
@@ -13,7 +13,10 @@ describe StonesUser do
   end
 
   it 'should return saved resources in its queue' do
-    expect(goal.queued_items).to eq([saved_resource])
+    # Checks that the numbers in the queue get the right elements.
+    SavedResource.stub(:find).with(1).and_return("This.")
+    SavedResource.stub(:find).with(2).and_return("That.")
+    goal.queued_items.should eq ["This.", "That."]
   end
 
 end

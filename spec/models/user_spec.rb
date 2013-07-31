535cfa4fbf44aee9aa9399c7932f2c60243c948e
Dean Tambling <dean.tambling@gmail.com>
2013-07-30 18:50:38 -0700

    Removed a test for a feature that no longer exists.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe User do
-	
+
 	let(:message) { FactoryGirl.create(:message) }
 	let(:user1) { message.from }
 	let(:user2) { message.to }
-	
+
 	let(:goal) { FactoryGirl.create(:stones_user) }
 	let(:resources_stone) { FactoryGirl.create(:resources_stone) }
 	let(:saved_resource) { FactoryGirl.create(:saved_resource, stones_user: goal, resources_stone: resources_stone) }
@@ -31,10 +31,6 @@ describe User do
 		it "should retrieve the users that this user has interacted with" do
 			expect(user1.message_partners.size).to eq(1)
 			expect(user2.message_partners.size).to eq(1)
-		end
-
-		it "should get the next resource to be completed" do 
-			expect(user1.next_queued_resource).to eq(saved_resource)
 		end
 	end
 end

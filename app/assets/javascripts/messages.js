d147e00cc62f0150c11e6eb52c44cd85f7f25b8b
Hussain Tambawala <hussain283@gmail.com>
2013-08-01 14:34:34 -0700

    Fixed Messaging

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-  window.location.href = "#bottom"
+
   $('form#new_message').bind("ajax:success", function(evt, data, status, xhr){
-    if ($('form#new_message textarea').val()!=""){
-      $('#messages').append(data);
-      window.location.href = "#bottom"
-    }
+    $('#messages').append(data);
+    window.location.href = "#bottom";
     $('form#new_message textarea').val("");
   });
 });

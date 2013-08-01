1910ad0354494b14143e66b9d12b8ea73f41a8f3
Dean Tambling <dean.tambling@gmail.com>
2013-08-01 12:55:04 -0700

    Messages window now defaults to bottom of chat scroll.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-
+  window.location.href = "#bottom"
   $('form#new_message').bind("ajax:success", function(evt, data, status, xhr){
     if ($('form#new_message textarea').val()!=""){
       $('#messages').append(data);
+      window.location.href = "#bottom"
     }
     $('form#new_message textarea').val("");
   });

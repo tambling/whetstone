1aaae85d7ffde2dcea4b2313274067890e0195cf
Dean Tambling <dean.tambling@gmail.com>
2013-07-31 20:29:56 -0700

    Main page now has animation of our motto.

--- app/assets/javascripts/motto.js
+++ app/assets/javascripts/motto.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('.motto p').each(function(index){
+  $('.motto .item').each(function(index){
     var self = this
     window.setTimeout(function(){
-      $(self).fadeIn(500).delay(2000).fadeOut(500);
-    }, 3000*index);
+      $(self).fadeIn(500).delay(1000).fadeOut(600);
+    }, 2200*index);
   });
-  window.setTimeout(function(){$('.motto #logo').fadeIn();}, 9000);
+  window.setTimeout(function(){$('.motto #logo').fadeIn();}, 6600);
 });

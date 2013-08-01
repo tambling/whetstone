1c1167ea3a6c18a974383bd880140ae1f5836226
Dean Tambling <dean.tambling@gmail.com>
2013-08-01 09:54:27 -0700

    Small enhancements to messages.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
+
   $('form#new_message').bind("ajax:success", function(evt, data, status, xhr){
+    if ($('form#new_message textarea').val()!=""){
+      $('#messages').append(data);
+    }
     $('form#new_message textarea').val("");
-    $('#messages').append(data);
   });
 });

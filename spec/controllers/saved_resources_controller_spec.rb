210c86d8fd8825d5cbfd2be5bc7419389c1800f4
Jared Smith <jared.rit@me.com>
2013-07-31 20:44:55 -0700

    Saving for checkout

--- spec/controllers/saved_resources_controller_spec.rb
+++ spec/controllers/saved_resources_controller_spec.rb
@@ -9,10 +9,10 @@ describe SavedResourcesController do
 
 	describe "POST #create" do 
 		
-		context "user is signed in" do 
+		# context "user is signed in" do 
 			
 			before do
-				controller.stub(:user_signed_in?).and_return(true)
+				# controller.stub(:user_signed_in?).and_return(true)
 				controller.stub(:current_user).and_return(user)
 				controller.stub(:create_goal_for).and_return(test_goal)
 				controller.stub(:stone).and_return(stone)
@@ -30,20 +30,20 @@ describe SavedResourcesController do
 				expect(response.status).to eq 201
 			end
 
-		end
+		# end
 
-		context "when the user is not signed in" do 
-			before do
-				controller.stub(:user_signed_in?).and_return(false)
-			end
+		# context "when the user is not signed in" do 
+		# 	before do
+		# 		controller.stub(:user_signed_in?).and_return(false)
+		# 	end
 
-			it "should redirect to a 'user not signed in' page" do 
-				post :create
-				expect(ResourcesStone).not_to receive(:find)
-				expect(response.status).to eq 401
-			end
+		# 	it "should redirect to a 'user not signed in' page" do 
+		# 		post :create
+		# 		expect(ResourcesStone).not_to receive(:find)
+		# 		expect(response.status).to eq 401
+		# 	end
 		
-		end
+		# end
 
 	end
 

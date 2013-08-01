5dd7980e497384642bf486b0c5edd6d41ee276ee
Jared Smith <jared.rit@me.com>
2013-08-01 09:46:43 -0700

    Update gemfile.lock and remove last reference to split

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
-require "split"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
